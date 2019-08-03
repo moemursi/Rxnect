f5f6fa7e20d69719620dbfe3a50229c99f446a6b
MoeTech <jemkarry@live.com>
2019-08-03 16:22:22 -0400

    user model added

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+//connect database
+connectDB();
+
+
 app.get('/' ,(req , res) => res.send('API Running'))
+app.use('/api/users' , require('./routes/api/users'));
+app.use('/api/profile' , require('./routes/api/profile'));
+app.use('/api/post' , require('./routes/api/post'));
+app.use('/api/auth' , require('./routes/api/auth'));
+
+
+
 
 
 const PORT = process.env.PORT || 5000; 
 
 app.listen(PORT , () => console.log(`Server Started on port ${PORT}` )) ;
+
