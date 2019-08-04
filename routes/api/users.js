cf3a46f6d257aa45dae7a909f9353e3e51b8f3b5
MoeTech <jemkarry@live.com>
2019-08-03 21:01:35 -0400

    commit

--- routes/api/users.js
+++ routes/api/users.js
@@ -58,7 +58,7 @@ router.post(
     res.send("User Registered");
     } catch (err) {
       console.error(err.message);
-      res.status(500).send("Server Error ");
+      res.status(500).send("Server Error ") 
       
     }
     
