aa0d0575505834dfd7a39fc994df0c34743aedcd
MoeTech <jemkarry@live.com>
2019-08-03 22:14:53 -0400

    added auth token

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const gravatar = require('gravatar')
+const gravatar = require("gravatar");
 const { check, validationResult } = require("express-validator");
-const bcrypt = require('bcrypt.js')
-
-const User = require('../../models/User')
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../../models/User");
+const config = require("config");
 
 // @route   POST api/users
 // @desc    Register user
@@ -27,42 +28,49 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
-    const {name, email, password}  =  req.body;
+    const { name, email, password } = req.body;
     try {
       let user = await User.findOne({ email });
       if (user) {
-        res.status(400).json({ errors: [{ "msg" : "username is already taken"}] });
-      }    
+        res
+          .status(400)
+          .json({ errors: [{ msg: "username is already taken" }] });
+      }
 
-    const avatar = gravatar.url(email , {
-      s: '200',
-      r: 'pg',
-      d: 'mm'
-    })
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm"
+      });
 
-    user = new User({
-      name,
-      email,
-      avatar,
-      password
-    });
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
 
-    const salt = await bcrypt.gensalt(10);
+      const salt = await bcrypt.gensalt(10);
 
-    user.password = await bcrypt.hash(password , salt);
+      user.password = await bcrypt.hash(password, salt);
 
-    await user.save();
-
-
-    
-    res.send("User Registered");
+      await user.save();
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+      jwt.sign(payload, config.get("jwtToken"), { expiresIn: 36000 }),
+        () => {
+          (err, token) => {
+            if (err) throw err;
+            res.json({ token });
+          };
+        };
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server Error ");
-      
     }
-    
-    
   }
 );
 
