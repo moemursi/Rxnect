e85e2ec8d21ea17b490b4a6126cbb4df8170d1f5
MoeTech <jemkarry@live.com>
2019-08-03 20:38:21 -0400

    update user

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
 
+const User = require('../../models/User')
+
 // @route   POST api/users
 // @desc    Register user
 // @access  Public
@@ -17,20 +19,33 @@ router.post(
       "please enter a valid password with 6 or more characters"
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    //see if user exists
+
+    const {name, email, password}  =  req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        res.status(400).json({ [{ "msg" : "username is already taken"}] });
+      }    
+    
 
     //get user's gravatar
 
     //encrypt the password using bcrypt
 
     //return json webtoken
-
     res.send("user route is here ");
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error ")
+      
+    }
+    
+    
   }
 );
 
